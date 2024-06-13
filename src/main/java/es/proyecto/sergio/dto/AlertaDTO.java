75258e1ee4c99de0c083bc8a9a60dea34b950d90
Sergioig37 <sergioig37@educastur.es>
2024-06-13 11:01:06 +0200

    Recuperados los reports

--- src/main/java/es/proyecto/sergio/dto/AlertaDTO.java
+++ src/main/java/es/proyecto/sergio/dto/AlertaDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class AlertaDTO {
 
-    private String id;
+    private Long id;
 
     @NotEmpty(message = "El nombre no puede estar vacío")
     @Size(max = 255, min = 4, message = "El nombre debe tener como máximo 255 caracteres")
@@ -17,5 +17,5 @@ public class AlertaDTO {
     @Size(max = 255, min = 4, message = "La descripción debe tener como máximo 255 caracteres")
     private String descripcion;
 
-    private String numeroUsuarios;
+    private Long numeroUsuarios;
 }
